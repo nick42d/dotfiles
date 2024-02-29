699a809bd5ccdf2fc185df30d1a46fb8adb41de3
Nick Dowsett <nickdowsett42@gmail.com>
2024-02-29 22:08:54 +0800

    Changed to use hyprlock over swaylock

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 swayidle -w \
-    timeout 600 'swaylock -f --screenshots --fade-in 0.1 --effect-pixelate 15' \
+    timeout 600 'hyprlock' \
     timeout 900 'hyprctl dispatch dpms off' \
     resume 'hyprctl dispatch dpms on' \
-    before-sleep 'swaylock -f --screenshots --fade-in 0.1 --effect-pixelate 15'
+    before-sleep 'hyprlock'
